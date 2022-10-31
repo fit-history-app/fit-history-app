dd9595b418b1f760533a488881a4b3220ef2d939
Grant Butler <grantbutler7@gmail.com>
2022-10-31 19:02:20 -0400

    move out of src/home to src

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { 
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import "./index.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import Home from "./routes/home/main";
+import Trivia from "./routes/trivia/main";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Home />}
+      errorElement={<ErrorPage />}
+    >
+      <Route
+        path="trivia"
+        element={<Trivia />}
+        >
+        </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  </React.StrictMode>
+);
