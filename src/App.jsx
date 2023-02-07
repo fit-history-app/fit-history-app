19cca6d87ad0c1459e94f2bdbdcc08c7b2b64983
Tyler Zars <tyler@zars.me>
2023-02-07 18:13:40 -0500

    routing improvement/css and css movement

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { load } from 'js-yaml';
 import facts from './history.yaml';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import Directions from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidGVlbWFuMjIiLCJhIjoiY2w5ZDcxbWh0MDM4MTN3dDl3Nnk1bmh2MyJ9.kfti7m0R9PtYzfP-c7qK2Q';
@@ -67,8 +68,8 @@ export default function App() {
 
    var directions = new Directions({
       accessToken: 'pk.eyJ1IjoidGVlbWFuMjIiLCJhIjoiY2w5ZDcxbWh0MDM4MTN3dDl3Nnk1bmh2MyJ9.kfti7m0R9PtYzfP-c7qK2Q',
-      unit: 'metric',
-      profile: 'mapbox/cycling'
+      unit: 'imperial',
+      profile: 'mapbox/walking'
     });
 
    useEffect(() => {
@@ -80,7 +81,7 @@ export default function App() {
          zoom: zoom
       });
       map.current.addControl(geolocate); // Add Geolocate button
-      map.current.addControl(directions); // Add directions
+      map.current.addControl(directions, 'top-left'); // Add directions
    });
 
    useEffect(() => {
