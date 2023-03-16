c5548d1022fd01379073d8f72a6e7fd73ebb092c
Cameron Miskell <cam.a.miskell@gmail.com>
2023-03-16 17:19:48 -0400

    Create page header with menus

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import poiwaypoints from './poi_waypoints.json';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import Directions from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+import { element } from 'prop-types';
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidGVlbWFuMjIiLCJhIjoiY2w5ZDcxbWh0MDM4MTN3dDl3Nnk1bmh2MyJ9.kfti7m0R9PtYzfP-c7qK2Q';
@@ -196,10 +197,50 @@ export default function App() {
       directions.setDestination([-80.62430, 28.06280]); 
    }
 
-
-
    return (
       <div>
+
+         <div className='header'>
+            <h2>FL Tech History Tours</h2>
+            <div>
+               <ul className='menuBar'>
+                  <li>
+                     <div className='itemContainer' id='tour'>
+                        <button className='menuItem'>Tours</button>
+                        <div className='subMenuItem'>
+                           <p onClick={load_tour1}>Load Tour #1</p>
+                           <p onClick={load_tour2}>Load Tour #2</p>
+                           <p onClick={load_tour3}>Load Tour #3</p>
+                        </div>
+                     </div>
+                  </li>
+                  <li>
+                     <div className='itemContainer' id='games'>
+                        <button className='menuItem'>Games</button>
+                        <div className='subMenuItem'>
+                           <p>Scavenger Hunt</p>
+                           <p>History Trivia</p>
+                        </div>
+                     </div>
+                  </li>
+                  <li>
+                     <div className='itemContainer' id='sharing'>
+                        <button className='menuItem'>Social</button>
+                        <div className='subMenuItem'>
+                           <a href="https://www.facebook.com/login.php?skip_api_login=1&api_key=966242223397117&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fsharer%2Fsharer.php%3Fkid_directed_site%3D0%26u%3Dhttps%253A%252F%252Ffit-history-tours.live%252F%26display%3Dpopup%26ref%3Dplugin%26src%3Dshare_button&cancel_url=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fclose_window%2F%3Fapp_id%3D966242223397117%26connect%3D0%23_%3D_&display=popup&locale=en_US&kid_directed_site=0">
+                              <img src="facebook_svg_icon.svg" alt="Facebook" />
+                           </a>
+                           <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">
+                              
+                              <img src="twitter_svg_icon.svg" alt="Twitter" />
+                              </a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                        </div>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </div>
+
          <div className="map-sidebar">
             Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} | Florida Tech: {inFloridaTech}
          </div>
@@ -214,11 +255,11 @@ export default function App() {
             {lng && <p>Longitude: {lng}</p>}
          </div>
 
-         <div align="center">
+         {/* <div align="center">
             <button onClick={load_tour1}>Load Tour #1</button>
             <button onClick={load_tour2}>Load Tour #2</button>
             <button onClick={load_tour3}>Load Tour #3</button>
-         </div>
+         </div> */}
       </div>
    );
 }
