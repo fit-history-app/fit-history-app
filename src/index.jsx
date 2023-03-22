0a18400a12a9ee1daa7b3030afa7bb1d22057640
Grant Butler <gronk.droid@gmail.com>
2023-03-22 18:23:57 -0400

    move styles

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import {
     RouterProvider
 } from "react-router-dom";
 import "mapbox-gl/dist/mapbox-gl.css";
-import "./index.css";
+import "./styles/index.css";
 
 // page imports
 import Root from "./routes/root";
@@ -23,4 +23,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <RouterProvider router={router} />
     </React.StrictMode>
-)
+);
